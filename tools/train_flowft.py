86fb57f519a99e9efa2c41a28aa27585b4fab8ed
郭嘉豪 <gjh_hust@foxmail.com>
2024-10-13 09:09:25 +0800

    "train gaoe5,gaode6"

--- tools/train_flowft.py
+++ tools/train_flowft.py
@@ -1,5 +1,5 @@
 import os
-device = [2]
+device = [0]
 if len(device)==1:
     os.environ["CUDA_VISIBLE_DEVICES"] = str(device[0]).lower() 
 from ultralytics.models import YOLOFT,YOLO
@@ -10,4 +10,4 @@ from ultralytics.models import YOLOFT,YOLO
 model = YOLOFT("config/yoloft_dev/yoloft-C_DCN-L.yaml").load("yolov8l.pt") #train yoloft-l
 # model = YOLO("./config/yolo/yolov8.yaml").load("yolov8l.pt") #train yolov8-l
 
-results = model.train(data="config/dataset_dev/Train_gaode5_Test_gaode6.yaml",cfg="config/train/orige_stream.yaml", batch=8, epochs=20, imgsz=896, device=device,workers = 6)
+results = model.train(data="config/dataset_dev/Train_gaode5_Test_gaode6.yaml",cfg="config/train/orige_stream.yaml", batch=8, epochs=21, imgsz=896, device=device,workers = 6)
